57d5ce8c0acf8ae3db148dd26c6022b6b8aa85ef
Shane Hendrickson <shane.hendrickson@gmail.com>
2022-08-08 18:25:19 -0400

    Built Contact Section

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const card = document.querySelector('.card-inner')
-
-card.addEventListener('mouseover', function () {
-  card.classList.toggle('is-flipped')
-})
-
-card.addEventListener('mouseout', function () {
-  card.classList.toggle('is-flipped')
-})
